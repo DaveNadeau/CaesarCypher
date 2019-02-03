8d9c8b780ad09b7b170961ff675c30c593e67e3a
Dave Nadeau <37254473+DaveNadeau@users.noreply.github.com>
2019-02-02 19:28:48 -0800

    basic (flawed) implementation

--- CaesarCypher/Main.cpp
+++ CaesarCypher/Main.cpp
@@ -1,13 +1,32 @@
 #include<iostream>
+#include<vector>
+#include<string>
 
 
+std::vector<int> decode(std::string str);
 
 
 
 int main() {
+	std::string input;
+	std::vector<int> result;
+	std::cout << "Please enter the code: ";
 
+	std::cin >> input;
 
+	std::cout << result;
 
 	std::getchar();
 	return 0;
+}
+
+std::vector<int> decode(std::string str) {
+	std::vector<int> decodeArray = {};
+
+	for (int i = 0; i < str.length; i++) {
+		decodeArray.push_back(str[i]);
+	}
+
+	return decodeArray;
+
 }
